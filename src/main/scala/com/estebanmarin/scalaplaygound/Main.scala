89d7a06a38369e844863148a6ae781e713e68cd8
esteban <esteban.marin@adidas.com>
2021-08-17 18:33:12 -0500

    higher order functions

--- src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
+++ src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
@@ -5,31 +5,58 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("─" * 50)
 
-    {
-      val degreesInACircle = 360
-      println("I am in modulo")
-
-      350 until 370 foreach { i =>
-        println(i % degreesInACircle)
-      }
-    }
-
-    {
-
-      val f: Int => Int = _ + 5
-      val g: Int => Int = f
-
-      println(f equals f)
-      println(g equals f)
-      println(g(10))
-
-      println(f(g(10)))
-      val composed = f compose g
-
-      println(composed(10))
-    }
+    code(args)
 
     println("─" * 50)
+
+  }
+
+  def code(args: Array[String]): Unit = {
+
+    def show(
+        fName: String,
+        f: Int => Int,
+        arg: Int,
+      ): String =
+      s"""|Function: ${fName}\tArgument: ${arg}\tResult: ${f(arg)}""".stripMargin
+
+    def showRange(
+        fName: String,
+        arguments: Range,
+        f: Int => Int,
+      ): Unit =
+      arguments foreach { argument =>
+        println(
+          show(fName, f, argument)
+        )
+      }
+
+    def factorial(n: Int): Int = {
+      @scala.annotation.tailrec
+      def loop(acc: Int, n: Int): Int =
+        if (n <= 1)
+          acc
+        else
+          loop(n * acc, n - 1)
+      loop(1, n)
+    }
+
+    def fibonacci(n: Int): Int = {
+      @scala.annotation.tailrec
+      def loop(
+          n: Int,
+          a: Int,
+          b: Int,
+        ): Int = n match {
+        case 0 => a
+        case _ => loop(n - 1, b, a + b)
+      }
+      loop(n, 0, 1)
+    }
+
+    showRange(fName = " + 5", 1 to 10, f = _ + 5)
+    showRange(fName = "Factorial", 1 to 10, f = factorial)
+    showRange(fName = "Fibonacci", 1 to 10, f = fibonacci)
 
   }
 }
