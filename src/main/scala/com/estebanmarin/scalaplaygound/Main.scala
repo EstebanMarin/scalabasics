9ce6ecf81a6aa0df03a5ce321e507f31005af09d
esteban <esteban.marin@adidas.com>
2021-08-17 11:46:07 -0500

    Primitive types

--- src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
+++ src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
@@ -9,25 +9,60 @@ object Main {
       unsignedBits(n) + 1
 
     def unsignedBits(n: Long): Int =
-      toBinary(n).size
+      n.toBinaryString.size
 
-    def toBinary(x: Long): String = {
-
-      @scala.annotation.tailrec
-      def loop(x: Long, acc: String): String =
-        if (x <= 1)
-          s"${(x % 2).toString()}${acc}"
-        else
-          loop(
-            x = x / 2,
-            acc = s"${(x % 2).toString()}${acc}",
-          )
-
-      loop(x, "")
+    def rendered(
+        `type`: String,
+        min: Long,
+        max: Long,
+        bits: Int,
+      ) = {
+      val bytes = bits / 8
+      s"${`type`}\thas $bits\t binary digits (bits), which is $bytes bytes with \tpossible values: $min .. $max"
     }
 
-    println(toBinary(73))
-    println(unsignedBits(73))
+    def renderedSigned(
+        `type`: String,
+        min: Long,
+        max: Long,
+      ): String =
+      rendered(`type`, min, max, signedBites(max))
+
+    def renderedUnsigned(
+        `type`: String,
+        min: Long,
+        max: Long,
+      ): String =
+      rendered(`type`, min, max, unsignedBits(max))
+
+    def renderFloating(
+        `type`: String,
+        min: Double,
+        max: Double,
+      ): String =
+      s"${renderFirstHalf(`type`)} $min .. $max"
+
+    def renderFirstHalf(`type`: String): String =
+      s"${`type`}\t\t\t\t\t\t\t\tpossible values:"
+    println(renderedSigned("Byte", Byte.MinValue, Byte.MaxValue))
+    println()
+
+    println(renderedSigned("Short", Short.MinValue, Short.MaxValue))
+    println(renderedUnsigned("Chart", Char.MinValue, Char.MaxValue))
+    println()
+
+    println(renderedSigned("Int", Int.MinValue, Int.MaxValue))
+    println(renderedSigned("Long", Long.MinValue, Long.MaxValue))
+    println()
+
+    println(renderFloating("Float", Float.MinValue, Float.MaxValue))
+    println(renderFloating("Double", Double.MinValue, Double.MaxValue))
+    println()
+
+    println(s"${renderFirstHalf("Boolean")}, ${true} and ${false}")
+    println()
+
+    println(s"${renderFirstHalf("Unit")}, ${()} and ${{}}")
 
     println("─" * 50)
   }
