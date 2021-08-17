e55bf445e0b5e1ceb0bf5c13047523113c3527a9
esteban <esteban.marin@adidas.com>
2021-08-17 15:22:12 -0500

    way of creation part 1

--- src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
+++ src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
@@ -5,65 +5,9 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("─" * 50)
 
-    def signedBites(n: Long): Int =
-      unsignedBits(n) + 1
+    val i: (Int, Int) => Boolean = _ == _
 
-    def unsignedBits(n: Long): Int =
-      n.toBinaryString.size
-
-    def rendered(
-        `type`: String,
-        min: Long,
-        max: Long,
-        bits: Int,
-      ) = {
-      val bytes = bits / 8
-      s"${`type`}\thas $bits\t binary digits (bits), which is $bytes bytes with \tpossible values: $min .. $max"
-    }
-
-    def renderedSigned(
-        `type`: String,
-        min: Long,
-        max: Long,
-      ): String =
-      rendered(`type`, min, max, signedBites(max))
-
-    def renderedUnsigned(
-        `type`: String,
-        min: Long,
-        max: Long,
-      ): String =
-      rendered(`type`, min, max, unsignedBits(max))
-
-    def renderFloating(
-        `type`: String,
-        min: Double,
-        max: Double,
-      ): String =
-      s"${renderFirstHalf(`type`)} $min .. $max"
-
-    def renderFirstHalf(`type`: String): String =
-      s"${`type`}\t\t\t\t\t\t\t\tpossible values:"
-    println(renderedSigned("Byte", Byte.MinValue, Byte.MaxValue))
-    println()
-
-    println(renderedSigned("Short", Short.MinValue, Short.MaxValue))
-    println(renderedUnsigned("Chart", Char.MinValue, Char.MaxValue))
-    println()
-
-    println(renderedSigned("Int", Int.MinValue, Int.MaxValue))
-    println(renderedSigned("Long", Long.MinValue, Long.MaxValue))
-    println()
-
-    println(renderFloating("Float", Float.MinValue, Float.MaxValue))
-    println(renderFloating("Double", Double.MinValue, Double.MaxValue))
-    println()
-
-    println(s"${renderFirstHalf("Boolean")}, ${true} and ${false}")
-    println()
-
-    println(s"${renderFirstHalf("Unit")}, ${()} and ${{}}")
-
+    println(i(10, 11))
     println("─" * 50)
   }
 }
