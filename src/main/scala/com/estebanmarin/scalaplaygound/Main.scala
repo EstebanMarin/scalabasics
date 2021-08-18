3916412111ebf5f8af1ba5af8c8fd7dcd1b331fa
esteban <esteban.marin@adidas.com>
2021-08-18 17:13:00 -0500

    working migrate now to unmitable

--- src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
+++ src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
@@ -13,12 +13,39 @@ object Main {
 
   def code(args: Array[String]): Unit = {
 
-    // Higher order because produces another function
-    // as it result but put type to trhe bound variable
-    def closeTerm(freeVariable: Int)(boundVariable: Int): Int =
-      freeVariable + boundVariable
+    def bankAccount(initialBalance: Int): Symbol => Int => Int = {
+      var balance: Int = initialBalance
 
-    println(closeTerm(freeVariable = 3)(boundVariable = 3))
+      val withdraw: Int => Int = amount =>
+        if (balance >= amount) {
+          balance = balance - amount
+          balance
+        }
+        else
+          sys.error(s"Insuficient funds")
 
+      val deposit: Int => Int = amount =>
+        if (balance >= 1) {
+          balance = balance + amount
+          balance
+        }
+        else
+          balance = amount + balance
+      balance
+
+      val dispatch: Symbol => (Int => Int) = operation =>
+        operation match {
+          case Symbol("withdraw") => withdraw
+          case Symbol("deposit")  => deposit
+          case _                  => x => x
+        }
+      dispatch
+    }
+
+    val bankAccount1 = bankAccount(initialBalance = 100)
+
+    println(bankAccount1(Symbol("withdraw"))(10))
+    println(bankAccount1(Symbol("withdraw"))(50))
+    println(bankAccount1(Symbol("deposit"))(50))
   }
 }
