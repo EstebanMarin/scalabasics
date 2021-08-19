062fe366d22133b1adda50ad2fd1b4df35b7be51
esteban <esteban.marin@adidas.com>
2021-08-19 18:06:01 -0500

    object orientation value proposition

--- src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
+++ src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
@@ -1,31 +1,6 @@
 package com.estebanmarin
 package scalaplaygound
 
-import Constants._
-import scala.concurrent.duration._
-
-object TickUntilEnterPressed {
-  def main(args: Array[String]): Unit =
-    Scala.tickUntilEnterPressed(Interval) {
-
-      Terminal.clearCanvas()
-      Terminal.goUp(LineToGoUp)
-      TwoDimensionalTime.showCurrent(TimePattern)
-    }
-}
-object TickForFewSeconds {
-  def main(args: Array[String]): Unit =
-    Scala.loop(times = FewSeconds) {
-      Terminal.clearCanvas()
-      Terminal.goUp(LineToGoUp)
-      TwoDimensionalTime.showCurrent(TimePattern)
-      Scala.wait(Interval)
-    }
-}
-
-object Constants {
-  val FewSeconds: Int = 30
-  val TimePattern: String = "HH:mm:ss"
-  val Interval: FiniteDuration = 1.second
-  val LineToGoUp: Int = Int.MaxValue
+object main {
+  def main(args: Array[String]): Unit = ???
 }
--- src/main/scala/com/estebanmarin/scalaplaygound/Terminal.scala
+++
@@ -1,6 +0,0 @@
-package com.estebanmarin.scalaplaygound
-
-object Terminal {
-  def clearCanvas(): Unit = println("\u001b[2J")
-  def goUp(lines: Int): Unit = println(s"\u001b[${lines}A")
-}
