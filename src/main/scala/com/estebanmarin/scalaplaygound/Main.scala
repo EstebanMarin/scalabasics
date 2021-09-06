50627358b0c4d269510dc93956442806552b3d77
esteban <esteban.marin@adidas.com>
2021-09-06 11:05:44 -0500

    error handling

--- src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
+++ src/main/scala/com/estebanmarin/scalaplaygound/Main.scala
@@ -12,15 +12,9 @@ object main {
   def code(args: Array[String]): Unit = {
 
     println("Hello")
-    val person = Person(name = "Bob", age = 27, isMale = true)
 
-    println(person)
-    println(Console.MAGENTA + ("-" * 50) + Console.RESET)
-
-    val human = Human(name = "Bob", age = 27, isMale = true)
-
-    println(human.isMale)
-    println(Human)
+    {}
+    println("End")
   }
 
 }
