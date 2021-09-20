f9e87e740dc16834ca7bcdf5cc0ed6fc9f926068
esteban <esteban.marin@adidas.com>
2021-09-20 17:36:23 -0500

    Part 4: Scafolding tests

---
+++ src/main/scala/com/estebanmarin/scalaplaygound/homegrown/collections/Set.scala
@@ -0,0 +1,3 @@
+package com.estebanmarin.scalaplaygound.homegrown.collections
+
+object Set
---
+++ src/test/scala/com/estebanmarin/scalaplaygound/homegrown/collections/Set.scala
@@ -0,0 +1,10 @@
+import com.estebanmarin.scalaplaygound.homegrown.collections._
+import org.scalatest.funsuite._
+import org.scalatest.matchers.should._
+import org.scalatest.matchers.Matcher
+
+class SetSuite extends AnyFunSuite with Matchers {
+  test("first test") {
+    1 shouldBe 1
+  }
+}
