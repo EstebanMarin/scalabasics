27ec03351ccfd98a35d7b68ebfcedb6834294deb
esteban <esteban.marin@adidas.com>
2021-09-20 19:36:26 -0500

    Part 4 test complaining

--- src/main/scala/com/estebanmarin/scalaplaygound/homegrown/collections/Set.scala
+++ src/main/scala/com/estebanmarin/scalaplaygound/homegrown/collections/Set.scala
@@ -1,5 +1,13 @@
 package com.estebanmarin.scalaplaygound.homegrown.collections
 
+trait Set extends (String => Boolean) {
+  final def add(input: String): Set = element => input == element || this(element)
+  final def remove(input: String): Set = element => input != element
+  final def union(that: Set): Set = element => this(element) || that(element)
+  final def intersection(that: Set): Set = element => this(element) && that(element)
+}
+
 object Set {
-  def empty: String => Boolean = input => false
+  lazy val empty: Set = input => false
+
 }
--- src/test/scala/com/estebanmarin/scalaplaygound/homegrown/collections/Set.scala
+++ src/test/scala/com/estebanmarin/scalaplaygound/homegrown/collections/Set.scala
@@ -4,8 +4,91 @@ import org.scalatest.matchers.should._
 
 class SetSuite extends AnyFunSuite with Matchers {
   test("apply on empty set should yield 0") {
-    Set.empty(randomCharacter) shouldBe false
+    Set.empty(randomString) shouldBe false
   }
-  private def randomCharacter: String =
+  test("add on an empty set should yield a new Set with one element") {
+    val first = randomString
+    val second = randomString
+    first should not be second
+    val set = Set.empty.add(first)
+    set(first) shouldBe true
+    set(second)
+  }
+  test("add on an non empty set should yield a new Set with two element") {
+    val first = randomString
+    val second = randomString
+    first should not be second
+    val set = Set.empty.add(first).add(second)
+    set(first) shouldBe true
+    set(second) shouldBe true
+  }
+  test("remove on a non empty Set should yield a new Set without the element") {
+    val element = randomString
+    val setWithElement = Set.empty.add(element)
+    setWithElement(element) shouldBe true
+    val setWithoutTheElement = setWithElement.remove(element)
+    setWithoutTheElement(element) shouldBe false
+  }
+  test("remove only the element in question") {
+    val first = randomString
+    val second = randomString
+    val setWithElement = Set.empty.add(first).add(second)
+    setWithElement(first) shouldBe true
+    setWithElement(second) shouldBe true
+    val setWithoutTheElement = setWithElement.remove(second)
+
+    setWithoutTheElement(first) shouldBe true
+    setWithoutTheElement(second) shouldBe false
+  }
+  test("remove only the element in question 2") {
+    val first = randomString
+    val second = randomString
+    val setWithElement = Set.empty.add(first).add(second)
+    setWithElement(first) shouldBe true
+    setWithElement(second) shouldBe true
+    val setWithoutTheElement = setWithElement.remove(first)
+
+    setWithoutTheElement(first) shouldBe false
+    setWithoutTheElement(second) shouldBe true
+  }
+  test("union on an empty set should yield an empty set") {
+    Set.empty.union(Set.empty)(randomString) shouldBe false
+  }
+  test(
+    "union on a non empty Set with an empty set should yield the original Set untouched"
+  ) {
+    val first = randomString
+    val second = randomString
+
+    first should not be second
+
+    val emptySet = Set.empty
+    val nonEmptySet = Set.empty.add(first).add(second)
+
+    emptySet.union(nonEmptySet)(first) shouldBe true
+    emptySet.union(nonEmptySet)(second) shouldBe true
+
+    nonEmptySet.union(emptySet)(first) shouldBe true
+    nonEmptySet.union(emptySet)(second) shouldBe false
+  }
+  test("union on non empty set should yield their union") {
+    val a = randomString
+    val b = randomString
+    val c = randomString
+    val d = randomString
+
+    val left = Set.empty.add(a).add(b)
+    val right = Set.empty.add(c).add(b)
+
+    val union = left.union(right)
+    union(a) shouldBe true
+    union(b) shouldBe true
+    union(c) shouldBe true
+    union(d) shouldBe true
+  }
+  test("intersection on empty Set shouild yield and empty Set") {
+    Set.empty.intersection(Set.empty)(randomString) shouldBe false
+  }
+  private def randomString: String =
     scala.util.Random.alphanumeric.take(5).mkString
 }
