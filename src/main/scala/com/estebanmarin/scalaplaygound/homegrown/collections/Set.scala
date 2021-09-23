db6ea00e1f87a6631ab5ec8a71ba51910775796b
esteban <esteban.marin@adidas.com>
2021-09-23 18:05:20 -0500

    part 10 - variance

--- src/main/scala/com/estebanmarin/scalaplaygound/homegrown/collections/Set.scala
+++ src/main/scala/com/estebanmarin/scalaplaygound/homegrown/collections/Set.scala
@@ -1,52 +1,71 @@
 package com.estebanmarin.scalaplaygound.homegrown.collections
 
-sealed trait Set[Element] extends (Element => Boolean) {
+sealed trait Set[+Element] /*extends (Element => Boolean)*/ {
   import Set._
-  final override def apply(input: Element): Boolean =
+
+  final /*override*/ def apply[Super >: Element](input: Super): Boolean =
     contains(input)
-  final def doesNotContain(input: Element): Boolean =
+
+  final def doesNotContain[Super >: Element](input: Super): Boolean =
     !contains(input)
-  final def contains(input: Element): Boolean =
+
+  final def contains[Super >: Element](input: Super): Boolean =
     exists(_ == input)
+
   final def doesNotExist(predicate: Element => Boolean): Boolean =
     !exists(predicate)
+
   final def exists(predicate: Element => Boolean): Boolean =
     fold(false)(_ || predicate(_))
+
   final def notForall(predicate: Element => Boolean): Boolean =
     !forall(predicate)
+
   final def forall(predicate: Element => Boolean): Boolean =
     fold(true)(_ && predicate(_))
 
-  final def add(input: Element): Set[Element] =
+  final def add[Super >: Element](input: Super): Set[Super] =
     fold(NonEmpty(input, empty)) { (acc, current) =>
-      if (current == input) acc else NonEmpty(current, acc)
+      if (current == input)
+        acc
+      else
+        NonEmpty(current, acc)
     }
 
-  final def remove(input: Element): Set[Element] =
-    fold(empty[Element]) { (acc, current) =>
-      if (current == input) acc else NonEmpty(current, acc)
+  final def remove[Super >: Element](input: Super): Set[Super] =
+    fold[Set[Super]](empty) { (acc, current) =>
+      if (current == input)
+        acc
+      else
+        NonEmpty(current, acc)
     }
 
-  final def union(that: Set[Element]): Set[Element] =
+  final def union[Super >: Element](that: Set[Super]): Set[Super] =
     fold(that)(_ add _)
 
-  final def intersection(that: Element => Boolean): Set[Element] =
-    filter(that)
+  final def intersection(predicate: Element => Boolean): Set[Element] =
+    filter(predicate)
 
   final def filter(predicate: Element => Boolean): Set[Element] =
-    fold(empty[Element]) { (acc, current) =>
-      if (predicate(current)) acc.add(current) else acc
+    fold[Set[Element]](empty) { (acc, current) =>
+      if (predicate(current))
+        acc.add(current)
+      else
+        acc
     }
 
   final def difference(predicate: Element => Boolean): Set[Element] =
-    fold(empty[Element]) { (acc, current) =>
-      if (predicate(current)) acc else acc.add(current)
+    fold[Set[Element]](empty) { (acc, current) =>
+      if (predicate(current))
+        acc
+      else
+        acc.add(current)
     }
 
   final def isSubsetOf(predicate: Element => Boolean): Boolean =
     forall(predicate)
 
-  final def isSupersetOf(that: Set[Element]): Boolean =
+  final def isSupersetOf[Super >: Element](that: Set[Super]): Boolean =
     that.isSubsetOf(this)
 
   final override def equals(other: Any): Boolean = other match {
@@ -55,35 +74,39 @@ sealed trait Set[Element] extends (Element => Boolean) {
   }
 
   final override def hashCode: Int =
-    fold(41)(_ + _.hashCode())
+    fold(41)(_ + _.hashCode)
 
   final override def toString: String =
     if (isEmpty)
       "{}"
     else {
       val otherElementsSplitByCommaSpace =
-        otherElementsOrThrowExcepetion.fold("") { (acc, current) =>
+        otherElementsOrThrowException.fold("") { (acc, current) =>
           s"$acc, $current"
         }
 
       "{ " + elementOrThrowException + otherElementsSplitByCommaSpace + " }"
     }
+
   final def size: Int =
     fold(0) { (acc, _) =>
       acc + 1
     }
 
   final def isEmpty: Boolean =
-    this.isInstanceOf[Empty[Element]]
+    this.isInstanceOf[Empty.type]
 
   final def nonEmpty: Boolean =
     !isEmpty
 
   final def isSingleton: Boolean =
-    nonEmpty && otherElementsOrThrowExcepetion.isEmpty
+    nonEmpty && otherElementsOrThrowException.isEmpty
 
   def sample: Option[Element] =
-    if (isEmpty) None else Some(elementOrThrowException)
+    if (isEmpty)
+      None
+    else
+      Some(elementOrThrowException)
 
   final def foreach[Result](function: Element => Result): Unit =
     fold(()) { (_, current) =>
@@ -91,31 +114,32 @@ sealed trait Set[Element] extends (Element => Boolean) {
     }
 
   final def map[Result](function: Element => Result): Set[Result] =
-    fold(empty[Result])(_ add function(_))
+    fold[Set[Result]](empty)(_ add function(_))
 
   final def flatMap[Result](function: Element => Set[Result]): Set[Result] =
-    fold(empty[Result]) { (acc, current) =>
+    fold[Set[Result]](empty) { (acc, current) =>
       function(current).fold(acc)(_ add _)
     }
 
   @scala.annotation.tailrec
-  final def fold[R](seed: R)(function: (R, Element) => R): R =
+  final def fold[Result](seed: Result)(function: (Result, Element) => Result): Result =
     if (isEmpty)
       seed
     else
-      otherElementsOrThrowExcepetion.fold[R](function(seed, elementOrThrowException))(function)
+      otherElementsOrThrowException.fold(function(seed, elementOrThrowException))(function)
 
-  private[this] lazy val (elementOrThrowException, otherElementsOrThrowExcepetion) = {
-    val nonEmptySet: NonEmpty[Element] = this.asInstanceOf[NonEmpty[Element]]
-    val element: Element = nonEmptySet.element
-    val otherElements: Set[Element] = nonEmptySet.otherElements
+  private[this] lazy val (elementOrThrowException, otherElementsOrThrowException) = {
+    val nonEmptySet = this.asInstanceOf[NonEmpty[Element]]
+    val element = nonEmptySet.element
+    val otherElements = nonEmptySet.otherElements
+
     element -> otherElements
   }
 }
 
 object Set {
   def apply[Element](element: Element, otherElements: Element*): Set[Element] =
-    otherElements.foldLeft(empty[Element].add(element))(_ add _)
+    otherElements.foldLeft[Set[Element]](empty.add(element))(_ add _)
 
   final private case class NonEmpty[Element](element: Element, otherElements: Set[Element])
       extends Set[Element]
@@ -125,7 +149,7 @@ object Set {
       patternMatchingNotSupported
   }
 
-  private class Empty[Element] extends Set[Element] {
+  private object Empty extends Set[Nothing] {
     private[this] def unapply(any: Any): Option[(String, Any)] =
       patternMatchingNotSupported
   }
@@ -136,5 +160,8 @@ object Set {
   private[this] def patternMatchingNotSupported: Nothing =
     sys.error("pattern matching on Sets is expensive and therefore not supported")
 
-  def empty[Element]: Set[Element] = new Empty[Element]
+  def empty: Set[Nothing] = Empty
+
+  implicit def SetCanBeUsedAsFunction1[Element](set: Set[Element]): Element => Boolean =
+    set.apply
 }
